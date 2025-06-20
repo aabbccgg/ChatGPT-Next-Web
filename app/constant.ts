4012a925ff3c34a596b851ec9aa6ba641aa8e238
aabbccgg <abkb@gmail.com>
2025-06-20 12:21:08 +0800

    Add gemini 2.5 stable

--- app/constant.ts
+++ app/constant.ts
@@ -544,7 +544,9 @@ const googleModels = [
   "gemini-2.0-flash-thinking-exp-01-21",
   "gemini-2.0-pro-exp",
   "gemini-2.0-pro-exp-02-05",
+  "gemini-2.5-flash",
   "gemini-2.5-flash-preview-05-20",
+  "gemini-2.5-pro",
   "gemini-2.5-pro-preview-06-05",
 ];
 
